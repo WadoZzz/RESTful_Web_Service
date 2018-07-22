9d943689f3a4011e89c801b2d5814f7a1fdb528a
Wadoz <vadikds@gmail.com>
2018-07-22 19:40:46 +0300

    Building project

---
+++ src/main/java/com/rest/wadoz/RESTful_Web_Service/ResTfulWebServiceApplication.java
@@ -0,0 +1,12 @@
+package com.rest.wadoz.RESTful_Web_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulWebServiceApplication.class, args);
+	}
+}
